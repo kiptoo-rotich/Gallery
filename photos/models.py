50a0bb84b1799a748c958d3d902a92e258881969
Softonic254 <rotichenoch27@gmail.com>
2021-07-02 13:52:00 +0300

    It adds class models for Imgaes

--- photos/models.py
+++ photos/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+class Image(models.Model):
+    image=models.ImageField(upload_to='images/')
+    image_name=models.CharField(max_length=40)
+    image_description=models.TextField(blank=False)
+    
