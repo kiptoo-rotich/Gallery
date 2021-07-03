7e201470bc455b85a2a8f8ba7dd8399126092362
Softonic254 <rotichenoch27@gmail.com>
2021-07-04 00:10:27 +0300

    It adds a search class method that returns the searched photo

--- photos/views.py
+++ photos/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
-from .models import Image,User
+from .models import Image,User,Category,Location
 import datetime as dt
+from django.http import Http404
 
 
 def photos(request):
     photos=Image.objects.all()
     date=dt.date.today()
     users=User.objects.all()
-    return render(request,'main/photos.html',{'photos':photos,'date':date})
+    category=Category.objects.all()
+    location=Location.objects.all()
+    return render(request,'main/photos.html',{'photos':photos,'date':date,'category':category,'location':location})
 
 def search(request):
-    if 'photo' in request.GET and request.GET['photo']:
-        search_term=request.GET('photo')
-        searched_photo=Image.search(search_term)
+    if 'Photo' in request.GET and request.GET['Photo']:
+        search_term=request.GET.get('Photo')
+        searched_photo=Category.search(search_term)
         message=f"{search_term}"
         
         return render(request,'main/search.html',{"message":message,"photos":searched_photo})
     else:
         message="You haven't searched for any term"
         return render(request,"main/search.html",{"message":message})
+    
+def photo(request,photo_id):
+    try:
+        photo=Image.objects.get(id=photo_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,'main/photo.html',{'photo':photo})
