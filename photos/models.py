afc881bc734867c2ecc83219d4fad659b00a2d28
Softonic254 <rotichenoch27@gmail.com>
2021-07-04 16:03:15 +0300

    It displays images on serached items

--- photos/models.py
+++ photos/models.py
@@ -55,7 +55,7 @@ class Category(models.Model):
 
     
     def __str__(self):
-        return self.category,self.user,self.image
+        return self.category
     
     @classmethod
     def search(cls,search_term):
